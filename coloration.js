814d9d2622ed2d1cc94df5ed72940615b1457513
Youssef El Saadany <youssef.el-saadany@unchartech.com>
2020-01-02 14:51:57 +0100

    js logic for sentiment value hover

---
+++ coloration.js
@@ -0,0 +1,25 @@
+const sentences = document.getElementsByClassName("sen");
+
+function fun(element) {
+    return function(e) {
+        let infoBlock = document.createElement("div");
+        infoBlock.setAttribute("id", "infoBlock");
+        infoBlock.setAttribute("class", "infoBlock");
+        infoBlock.innerHTML = "<h1>"+element.getAttribute("id")+"</h1>";
+        infoBlock.style.top = e.clientY + "px";
+        infoBlock.style.left = e.clientX + "px";
+        infoBlock.style.backgroundColor = "grey";
+        document.body.appendChild(infoBlock);
+    }
+}
+
+function removeInfo(e) {
+    const infoBlock = document.getElementById("infoBlock");
+    document.body.removeChild(infoBlock);
+}
+
+console.log(sentences);
+for (let sentence of sentences) {
+    sentence.addEventListener("mouseleave", removeInfo, false);
+    sentence.addEventListener("mouseenter", fun(sentence), false);
+}
