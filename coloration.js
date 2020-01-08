5fa963fa1a0585766668d113d29bd09c726ef618
Youssef El Saadany <youssef.el-saadany@unchartech.com>
2020-01-08 14:10:11 +0100

    rounded sentiment value

--- coloration.js
+++ coloration.js
@@ -5,7 +5,7 @@ function fun(element) {
     let infoBlock = document.createElement("div");
     infoBlock.setAttribute("id", "infoBlock");
     infoBlock.setAttribute("class", "infoBlock");
-    infoBlock.innerHTML = "<h1>" + element.getAttribute("id") + "</h1>";
+    infoBlock.innerHTML = "<h1>" + Math.round(element.getAttribute("id") * 100) / 100 + "</h1>";
     infoBlock.style.top = e.clientY + "px";
     infoBlock.style.left = e.clientX + "px";
     infoBlock.style.backgroundColor = "grey";
