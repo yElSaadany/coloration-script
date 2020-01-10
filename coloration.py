922ddc4f106c8943e27f4a2363d833819337bbc3
Youssef El Saadany <youssef.el-saadany@unchartech.com>
2020-01-10 17:06:42 +0100

    changed html for tests and added support to create multiple html from multiple docs and added support to create CSVs and XLSXs (sorry for the heavy commit)

--- coloration.py
+++ coloration.py
@@ -1,6 +1,9 @@
 import json
 import argparse
 import os
+# Pandas until something better
+from pandas.io.excel import ExcelWriter
+import pandas as pd
 
 
 def init_args():
@@ -8,43 +11,48 @@ def init_args():
     parser.add_argument("input", help="JSON file on which you have the sentiment analysis")
     parser.add_argument("-o", help="Output html file, will print to standard output if not selected")
     parser.add_argument("-t", help="HTML Template")
+    parser.add_argument("-m", help="Split documents into multiple HTMLs each named their document's id",
+                        action="store_true", default=False)
     args = parser.parse_args()
     return args
 
 
-def to_file(data, output, template=None):
+def to_file(data, output="id", template=None):
+    if output == "id":
+        output = data['id'] + ".html"
     with open(output, 'w') as html:
-        for document in data:
-            html.write("<!DOCTYPE html>")
-            html.write("<html>")
-            if template is None:
-                html.write("<head></head>")
-            else:
-                html.write("<head>")
-                html.write("<link rel='stylesheet' type='text/css' href='templates/%s.css'>" % template)
-                html.write("<link rel='stylesheet' type='text/css' href='style.css'>")
-                html.write("</head>")
-            html.write("<body><p><div id='info'></div>")
+        document = data
+        html.write("<!DOCTYPE html>")
+        html.write("<html>")
+        if template is None:
+            html.write("<head></head>")
+        else:
+            html.write("<head>")
+            html.write("<link rel='stylesheet' type='text/css' href='templates/%s.css'>" % template)
+            html.write("<link rel='stylesheet' type='text/css' href='style.css'>")
+            html.write("</head>")
+        html.write("<body><p><div id='info'></div>")
 
-            for line in document['sentences']:
-                if float(line['sentiment']) >= 0.75:
-                    a = 1 - float(line['sentiment'])
-                    a = 1 - a * float(4)
-                    if a < 0.1:
-                        a = 0.2
-                    green = 'background-color: rgba(0, 255, 0, %f); font-weight: bold' % a
-                    html.write("<span class='sen' id='%s' style='%s'>%s</span>\n" % (line['sentiment'], green, line['text']))
-                elif float(line['sentiment']) <= 0.25:
-                    a = 1 - float(line['sentiment']) * float(4)
-                    if a < 0.1:
-                        a = 0.2
-                    red = 'background-color: rgba(255, 0, 0, %f); font-weight: bold' % a
-                    html.write("<span class='sen' id='%s' style='%s'>%s</span>\n" % (line['sentiment'], red, line['text']))
-                else:
-                    html.write("<span id='%s'>%s</span>\n" % (line['id'], line['text']))
-            html.write("</p></body>")
-            html.write("<script src='coloration.js'></script>")
-            html.write("</html>")
+        for line in document['sentences']:
+            print(line)
+            if float(line['sentiment']) >= 0.75:
+                a = 1 - float(line['sentiment'])
+                a = 1 - a * float(4)
+                if a < 0.1:
+                    a = 0.2
+                green = 'background-color: rgba(0, 255, 0, %f); font-weight: bold' % a
+                html.write("<span class='sen' id='%s' style='%s'>%s</span>\n" % (line['sentiment'], green, line['text']))
+            elif float(line['sentiment']) <= 0.25:
+                a = 1 - float(line['sentiment']) * float(4)
+                if a < 0.1:
+                    a = 0.2
+                red = 'background-color: rgba(255, 0, 0, %f); font-weight: bold' % a
+                html.write("<span class='sen' id='%s' style='%s'>%s</span>\n" % (line['sentiment'], red, line['text']))
+            else:
+                html.write("<span id='%s'>%s</span>\n" % (line['sentiment'], line['text']))
+        html.write("</p></body>")
+        html.write("<script src='coloration.js'></script>")
+        html.write("</html>")
 
 
 def only_html(data):
@@ -84,6 +92,64 @@ def generate_multiple_html(folder, output_folder):
         to_file(news, name, "default")
 
 
+def multiple_html_json(documents, output_folder="results"):
+    for doc in documents:
+        to_file(doc, "%s/%s.html" % (output_folder, doc["id"]), "default")
+
+
+def from_csv_to_excel(csv, id):
+    try:
+        with ExcelWriter(id+'.xlsx') as ew:
+            pandas.read_csv(csv).to_excel(ew, sheet_name="sheet", index=None)
+    except:
+        pass
+
+
+def to_csv(json, output_folder='results_csv', excel=False):
+    documents = get_news_data(json)
+    for document in documents:
+        output_file = output_folder + '/' + document['id'] + '.csv'
+        with open(output_file, 'w+') as csv:
+            for sentence in document['sentences']:
+                line = '%s,%s,%f,0\n' % (sentence['id'],
+                                       sentence['text'],
+                                       sentence['sentiment'])
+                csv.write(line)
+        from_csv_to_excel(output_file, 'results_excel' + '/' + document['id'])
+
+
+def load_dataframe(json):
+    documents = get_news_data(json)
+    frames = []
+    id = []
+    for document in documents:
+        df = {'id': [], 'sentence': [], 'pred': [], 'human': []}
+        for sentence in document['sentences']:
+            df['id'].append(sentence['id'])
+            df['sentence'].append(sentence['text'])
+            df['pred'].append(sentence['sentiment'])
+            df['human'].append(0)
+        id.append(document['id'])
+        frames.append(pd.DataFrame(df))
+    return id, frames
+
+
+def store_to_file(json, csv=False, excel=False):
+    id, frames = load_dataframe(json)
+
+    i = 0
+    if excel:
+        for frame in frames:
+            frame.to_excel('results_excel/'+id[i]+'.xlsx', index=False)
+            i += 1
+
+    i = 0
+    if csv:
+        for frame in frames:
+            frame.to_csv('results_csv/'+id[i]+'.csv', index=False)
+            i += 1
+
+
 if __name__ == '__main__':
     args = init_args()
 
@@ -94,7 +160,11 @@ if __name__ == '__main__':
         generate_multiple_html(args.input, "results")
     else:
         data = get_news_data(args.input)
-        if args.o is not None:
+        if args.m:
+            if not os.path.isdir("results"):
+                os.mkdir("results")
+            multiple_html_json(data)
+        elif args.o is not None:
             to_file(data, args.o, args.t)
         else:
             only_html(data)
